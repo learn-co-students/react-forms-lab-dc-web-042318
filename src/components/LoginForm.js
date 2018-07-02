91a6ec5e388f8b9054c00fa3b5205ac04d6ece71
jtorre20 <jtorres1989@gmail.com>
2018-07-02 14:42:02 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,60 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  handleSubmit = ( event ) => {
+    event.preventDefault();
+    const {username, password} = this.state
+
+    // const username = this.state.username
+    // const password = this.state.password
+
+    if (!username || !password) {
+      return;
+    }
+
+    this.props.onSubmit({username, password});
+  };
+
+  handleUsernameChange = event => {
+    console.log('in username change', event.target.value)
+    this.setState({username: event.target.value});
+  };
+
+  handlePasswordChange = event => {
+    this.setState({password: event.target.value});
+  };
+
+  // handleInputChange = (event) => {
+  //   const {name, value} = event.target    
+    
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text"  name ="username" value={this.state.username} onChange={this.handleUsernameChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" >Log in</button>
         </div>
       </form>
     );
@@ -31,3 +65,10 @@ class LoginForm extends React.Component {
 }
 
 export default LoginForm;
+
+
+// const HogTile = ({ name, handleClick }) => {
+
+
+//   return( <li onClick={handleClick}> {name} </li>)
+// }
